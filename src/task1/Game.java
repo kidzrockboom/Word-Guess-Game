ecd9d12f81f9f5c8e4c8548ec67dc7845abec079
Richard Obaze <richardobaze@yahoo.co.uk>
2022-01-06 19:00:05 +0100

    Added the background functionality for drawBoard, random word, random list of chars etc

--- src/task1/Game.java
+++ src/task1/Game.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.Random;
+
 
 public class Game {
 
@@ -35,32 +37,43 @@ public class Game {
         /* No duplicates encountered, return true */
         return true;
     }
-
-    public static void fakeTable() {
-        String[][] swampGrid = new String[5][5];
-        for (int row = 0; row < 5; row++) {
-            // the row number
-            System.out.print((row % 5) + " ");
-            for (int column = 0; column < 5; column++) {
-                swampGrid[row][column] = "? ";
-                System.out.print(swampGrid[row][column]);
+    public static String[][] showTable(String[][] table) {
+        
+        for (int i = 0; i < 5; i++) {
+            System.out.print((i % 5) + "  ");
+            for (int j = 0; j < 5; j++) {
+                System.out.print(table[i][j] + " ");
             }
             System.out.println();
         }
-        // at the end the column
         System.out.print("  ");  // to skip de row number 
         for (int column = 0; column < 5; column++) {
-            System.out.print((column % 5) + " ");
+            System.out.print(" " + (column % 5) + " ");
         }
         System.out.println();
+        return table;
+    }
+    
+    public static String randomStrings() {
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        StringBuilder str = new StringBuilder();
+        Random rand = new Random();
+        while (str.length() < 27) { // length of the random string.
+            int index = (int) (rand.nextFloat() * alphabet.length());
+            str.append(alphabet.charAt(index));
+        }
+        String randomString = str.toString();
+        return randomString;
 
     }
-
-    public static void main(String[] args) {
+    
+    public static String keyWord(){
+        String keyWord = "";
+        Random rand = new Random();
 
         int count = 0;
         Game obj = new Game();
-        String[] word = new String[5665];
+        String[] words = new String[5665];
 
         String fileLocation = System.getProperty("user.dir");
 
@@ -73,14 +86,58 @@ public class Game {
             while (scanner.hasNextLine()) {
                 String data = scanner.nextLine();
                 if (obj.uniqueCharacters(data)) {
-                    word[count] = data;
+                    words[count] = data;
                     count++;
                 }
             }
         } catch (FileNotFoundException e) {
             System.out.println("An error Occurred, File not found.");
         }
-        fakeTable();
+        
+        int index = (int) (rand.nextFloat() * words.length);
+        keyWord = words[index];
+        
+        return keyWord;
+    }
+
+    public static String[][] buildTable() {
+        
+        int a = 0, b = 0;
+        
+        int wordStart = (int) a + b;
+        String randomStrings = randomStrings();
+        String keyWord = keyWord(); 
+        
+        String[][] grid = new String[5][5];
+        for (int row = 0; row < 5; row++) {
+            // the row number
+            System.out.print((row % 5) + " ");
+            for (int column = 0; column < 5; column++) {
+                grid[row][column] = "? ";
+                System.out.print(grid[row][column]);
+            }
+            System.out.println();
+        }
+        // at the end the column
+        System.out.print("  ");  // to skip de row number 
+        for (int column = 0; column < 5; column++) {
+            System.out.print((column % 5) + " ");
+        }
+        System.out.println();
+
+        return grid;
+    }
+
+    public static void main(String[] args) {
+                
+        String[][] fakeTable = {{"? ", "? ", "? ", "? ", "? "},
+                                {"? ", "? ", "? ", "? ", "? "},
+                                {"? ", "? ", "? ", "? ", "? "},
+                                {"? ", "? ", "? ", "? ", "? "},
+                                {"? ", "? ", "? ", "? ", "? "}
+        };
+        
+        showTable(fakeTable);
 
     }
 
